e254383c8f9a303b78d3f27f2a270f2412a3fb0f
wetooa <derpykidyt@gmail.com>
2025-03-07 17:10:27 +0800

    added leaderboard, improved db

---
+++ app/api/leaderboard/route.ts
@@ -0,0 +1,55 @@
+import { GS } from "@/db/db";
+import { Leaderboard } from "@/types/types";
+import { NextResponse } from "next/server";
+import { authOptions } from "../[...nextauth]";
+import { getServerSession } from "next-auth";
+
+export async function GET() {
+  try {
+    const data = await GS.getSheetData("leaderboard");
+    const sheet = await data.getRows();
+
+    const leaderboard: Leaderboard[] = sheet.map((row) => {
+      return {
+        id: row.get("id"),
+        departmentId: row.get("departmentId"),
+        category: row.get("category"),
+        wins: row.get("wins"),
+        losses: row.get("losses"),
+        draws: row.get("draws"),
+        points: row.get("points"),
+        createdOn: row.get("createdOn"),
+        updatedOn: row.get("updatedOn"),
+      };
+    });
+    return NextResponse.json({ leaderboard }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const reqData = (await req.json()) as Leaderboard;
+    const { id } = reqData;
+
+    const data = await GS.getSheetData("leaderboard");
+    const rows = await data.getRows();
+    const rowIndex = rows.findIndex((row) => row.get("id") === id);
+
+    rows[rowIndex].assign(reqData);
+    await rows[rowIndex].save();
+
+    return NextResponse.json({
+      message: "Row updated successfully!",
+      updatedRow: reqData,
+    });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
