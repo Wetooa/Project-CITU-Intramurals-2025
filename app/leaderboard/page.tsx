0e6b4ea41ebd86e42d001e0d3cb6906e8488db03
eloe <masterthemanok@gmail.com>
2025-03-07 16:58:01 +0800

    basic additions

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,16 @@
+import GameFiltersSchedule from "@/components/feature/game-filters";
+
+export default function LeaderBoardScreen() {
+  return (
+    <>
+      <div className="w-full h-full flex flex-col justify-center items-center px-5p">
+        <div className="w-full text-left">
+          <p className="text-7xl text-white font-bold ">LEADERBOARD</p>
+        </div>
+        <div>
+          <p>Highlights</p>
+        </div>
+      </div>
+    </>
+  );
+}
