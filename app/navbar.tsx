e925a5966f9fd2f635c96082033f19f41698891f
drkcutie <derrick.binangbang@cit.edu>
2025-03-07 09:10:32 +0800

    add line below nav

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -5,47 +5,43 @@ import { usePathname } from "next/navigation";
 
 export function NavBar() {
   return (
-    <nav className="w-full h-16 flex align-middle justify-between bg-[#262524] shadow-[0px_9px_0px_0px_rgba(46,_45,_44,_0.1)] p-4 box-border">
-      <img
-        className="h-auto max-w-full"
-        src="./thetechnologian_logo.png"
-        alt="image description"
-      ></img>
+      <nav className="w-full h-16 flex align-middle justify-between bg-[#262524] shadow-[0px_9px_0px_0px_rgba(46,_45,_44,_0.1)] p-4 box-border">
+        <img
+            className="h-auto max-w-full"
+            src="./thetechnologian_logo.png"
+            alt="image description"
+        />
 
-      <div className="flex h-full w-1/2 align-middle gap-16 justify-end pr-12">
-        <NavLink path="home"></NavLink>
-        <NavLink path="SCHEDULE"></NavLink>
-        <NavLink path="LEADERBOARD"></NavLink>
-        <NavLink path="BRACKET"></NavLink>
-      </div>
-    </nav>
+        <div className="flex h-full w-1/2 items-center gap-16 justify-end pr-12">
+          <NavLink path="home" />
+          <NavLink path="schedule" />
+          <NavLink path="leaderboard" />
+          <NavLink path="bracket" />
+        </div>
+      </nav>
   );
 }
 
 function NavLink({ path }: { path: string }) {
-  var pathname = usePathname();
+  const pathname = usePathname();
 
-  pathname = pathname.replace("/", "");
+  const isActive =
+      pathname === `/${path.toLowerCase()}` ||
+      (path.toLowerCase() === "home" && pathname === "/");
 
-  //   alert(pathname);
-
-  if (
-    pathname == path.toLowerCase() ||
-    (path.toUpperCase() == "HOME" && pathname === "/")
-  ) {
-    return (
-      <a className="font-bold text-center border-b-8 border-[#FF4747]">
-        {path.toUpperCase()}
-      </a>
-    );
-  } else {
-    return (
+  return (
       <Link
-        className="font-bold hover:cursor-pointer  hover:text-[#FF4747]"
-        href={path.toLowerCase()}
+          className={`font-bold text-center relative transition-all duration-300 ${
+              isActive ? "text-[#FF4747]" : "hover:text-[#FF4747]"
+          }`}
+          href={path.toLowerCase() === "home" ? "/" : `/${path.toLowerCase()}`}
       >
         {path.toUpperCase()}
+        <span
+            className={`absolute bottom-[-8px] left-0 w-full h-[4px] transition-all duration-300 ${
+                isActive ? "bg-[#FF4747]" : "hover:bg-[#FF4747] opacity-0 hover:opacity-100"
+            }`}
+        />
       </Link>
-    );
-  }
+  );
 }
