f695cdddd1a31c2560754b23fd459113f7146ffd
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2025-03-11 13:57:22 +0800

    Added round and winner filter

--- app/api/schedule/filter/route.ts
+++ app/api/schedule/filter/route.ts
@@ -9,9 +9,10 @@ export async function GET(req: Request) {
     const team2Id = searchParams.get("team2Id") || "";
     const matchDate = searchParams.get("matchDate") || "";
     const category = searchParams.get("category") || "";
-    const venue = searchParams.get("veneu") || "";
+    const venue = searchParams.get("venue") || "";
     const status = searchParams.get("status") || "";
     const round = searchParams.get("round") || "";
+    const winner = searchParams.get("winner") || "";
 
     const data = await GS.getSheetData("schedule");
     const sheet = await data.getRows();
@@ -24,7 +25,8 @@ export async function GET(req: Request) {
         (category === "" || row.get("category") === category) &&
         (venue === "" || row.get("venue") === venue) &&
         (status === "" || row.get("status") === status) &&
-        (status === "" || row.get("round") === status)
+        (round === "" || row.get("round") === round) && 
+        (winner === "" || row.get("winner") === winner)
       );
     });
 
