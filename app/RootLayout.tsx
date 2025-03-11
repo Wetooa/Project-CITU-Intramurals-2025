822fc2afe15e094c2b414cf4e6e3b3fb211f3e9b
drkcutie <derrick.binangbang@cit.edu>
2025-03-12 03:01:22 +0800

    fix nav

--- app/RootLayout.tsx
+++ app/RootLayout.tsx
@@ -1,54 +1,54 @@
 "use client";
 
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AnimatePresence, motion } from "framer-motion";
-import { Inter } from "next/font/google";
-import { usePathname } from "next/navigation";
+import {ThemeProvider} from "@/components/theme-provider";
+import {Toaster} from "@/components/ui/sonner";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {AnimatePresence, motion} from "framer-motion";
+import {Inter} from "next/font/google";
+import {usePathname} from "next/navigation";
 import "./globals.css";
-import { NavBar } from "./navbar";
+import {NavBar} from "./navbar";
 
 const inter = Inter({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const queryClient = new QueryClient();
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  const pathname = usePathname();
+    const pathname = usePathname();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <html lang="en">
-        <body
-          className={`${inter.variable} antialiased  text-white bg-charcoal_black`}
-        >
-          <NavBar />
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={pathname}
-              initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3, ease: "easeIn" }}
+    return (
+        <QueryClientProvider client={queryClient}>
+            <html lang="en">
+            <body
+                className={`${inter.variable} antialiased  text-white bg-charcoal_black mt-20`}
             >
-              <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                disableTransitionOnChange
-              >
-                {children}
-                <Toaster />
-              </ThemeProvider>
-            </motion.div>
-          </AnimatePresence>
-        </body>
-      </html>
-    </QueryClientProvider>
-  );
+            <NavBar/>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    key={pathname}
+                    initial={{opacity: 0, y: 10}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{duration: 0.3, ease: "easeIn"}}
+                >
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="dark"
+                        disableTransitionOnChange
+                    >
+                        {children}
+                        <Toaster/>
+                    </ThemeProvider>
+                </motion.div>
+            </AnimatePresence>
+            </body>
+            </html>
+        </QueryClientProvider>
+    );
 }
