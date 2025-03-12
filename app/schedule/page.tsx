13ce4eccd4d6129fa9d7770cf069885e493baaf0
drkcutie <derrick.binangbang@cit.edu>
2025-03-12 13:16:33 +0800

    fix sched text

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,38 +1,38 @@
 "use client";
 import GameFiltersSchedule from "@/components/feature/game-filters";
 import DayResultContainer from "@/components/feature/game-result-card";
-import { Schedule } from "@/types/types";
-import { useQuery } from "@tanstack/react-query";
-import { createContext, useEffect, useState } from "react";
+import {Schedule} from "@/types/types";
+import {useQuery} from "@tanstack/react-query";
+import {createContext, useEffect, useState} from "react";
 
 // Context Type
 interface ScheduleContextType {
-  data: Schedule[] | undefined;
-  isLoading: boolean;
-  isError: boolean;
-  filters: Filters;
-  setFilters: (filters: Filters) => void;
+    data: Schedule[] | undefined;
+    isLoading: boolean;
+    isError: boolean;
+    filters: Filters;
+    setFilters: (filters: Filters) => void;
 }
 
 export interface Filters {
-  category?: string;
-  matchDate?: string;
-  team1Id?: string;
-  team2Id?: string;
-  venue?: string;
-  gender?: string;
+    category?: string;
+    matchDate?: string;
+    team1Id?: string;
+    team2Id?: string;
+    venue?: string;
+    gender?: string;
 }
 
 // Create Context
 const ScheduleContext = createContext<ScheduleContextType | undefined>(
-  undefined,
+    undefined,
 );
 
 async function fetchSchedule(filters: Filters) {
-  const queryParams = new URLSearchParams(filters as Record<string, string>);
-  const response = await fetch(`/api/schedule/filter?${queryParams}`);
-  console.log("fetching data");
-  return response.json();
+    const queryParams = new URLSearchParams(filters as Record<string, string>);
+    const response = await fetch(`/api/schedule/filter?${queryParams}`);
+    console.log("fetching data");
+    return response.json();
 }
 
 // export function useSchedule() {
@@ -44,38 +44,38 @@ async function fetchSchedule(filters: Filters) {
 // }
 
 export default function ScheduleScreen() {
-  const [filters, setFilters] = useState<Filters>({
-    category: "",
-    matchDate: "",
-    team1Id: "",
-    team2Id: "",
-    venue: "",
-  });
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["schedule", JSON.stringify(filters)],
-    queryFn: () => fetchSchedule(filters),
-    refetchIntervalInBackground: false,
-    refetchOnMount: false,
-    enabled: !!filters,
-    refetchOnWindowFocus: false,
-  });
-  useEffect(() => {
-    console.log("Data: ", data);
-  }, [data]);
-  return (
-    <ScheduleContext.Provider
-      value={{ data, isLoading, isError, filters, setFilters }}
-    >
-      <div className="2xl:relative w-full h-full flex flex-col gap-5 justify-center items-center p-10">
-        <p className=" xl:text-4xl xl: 2xl:absolute 2xl:left-10 2xl:top-20 text-white font-bold text-4xl 2xl:text-8xl">
-          Schedule
-        </p>
-        <GameFiltersSchedule filters={filters} setFilters={setFilters} />
-        <DayResultContainer
-          schedule={data ? data.schedule : []}
-          isLoading={isLoading}
-        />
-      </div>
-    </ScheduleContext.Provider>
-  );
+    const [filters, setFilters] = useState<Filters>({
+        category: "",
+        matchDate: "",
+        team1Id: "",
+        team2Id: "",
+        venue: "",
+    });
+    const {data, isLoading, isError} = useQuery({
+        queryKey: ["schedule", JSON.stringify(filters)],
+        queryFn: () => fetchSchedule(filters),
+        refetchIntervalInBackground: false,
+        refetchOnMount: false,
+        enabled: !!filters,
+        refetchOnWindowFocus: false,
+    });
+    useEffect(() => {
+        console.log("Data: ", data);
+    }, [data]);
+    return (
+        <ScheduleContext.Provider
+            value={{data, isLoading, isError, filters, setFilters}}
+        >
+            <div className="2xl:relative w-full h-full flex flex-col gap-5 justify-center items-center p-10">
+                <p className=" xl:text-4xl xl: 2xl:absolute 2xl:left-10 2xl:top-20 text-white font-bold text-4xl 2xl:text-7xl">
+                    Schedule
+                </p>
+                <GameFiltersSchedule filters={filters} setFilters={setFilters}/>
+                <DayResultContainer
+                    schedule={data ? data.schedule : []}
+                    isLoading={isLoading}
+                />
+            </div>
+        </ScheduleContext.Provider>
+    );
 }
